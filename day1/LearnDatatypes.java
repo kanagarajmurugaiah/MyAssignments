9ef4ae86743d9081069e6bdbc911a91659dec72c
kanagaraj <kanagarajmurugaiah@gmail.com>
2025-02-01 20:31:41 +0530

    Add files via upload
    Day1 Commit Changes

---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte age = 31;
+		System.out.println(age);
+		
+		
+		short no = 652;
+		
+		int num;
+		System.out.println(num=age+no);
+	}
+
+}
